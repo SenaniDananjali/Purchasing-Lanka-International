02027b4df5afb7c4c1d0007b494fe82022e931fa
SenaniDananjali <senanidananjali@gmail.com>
2019-01-20 00:38:43 +0530

    -update urls
    Signed-off-by: SenaniDananjali <senanidananjali@gmail.com>

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,62 +11,62 @@ export class DataService {
   }
 
   getNames() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getStylistDetails')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getStylistDetails')
       .map(res => res.json());
 
   }
 
   getSkills() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getSkills')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getSkills')
       .map(res => res.json());
 
   }
 
   getProfPic() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getPorfilePic')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getPorfilePic')
       .map(res => res.json());
 
   }
 
   getStylistSkills() {
-    return this.http.get('http://localhost/backend/src/api.php?f=getStylistSkills')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getStylistSkills')
       .map(res => res.json());
 
   }
 
   getBusyDates() {
-    return this.http.get('http://localhost/backend/src/api.php?f=getBusyDates')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getBusyDates')
       .map(res => res.json());
 
   }
 
   getLocations() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getPreferredLocations')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getPreferredLocations')
       .map(res => res.json());
   }
 
   getStylistJob() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getJobRole')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getJobRole')
       .map(res => res.json());
   }
 
   getJob() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=job')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=job')
       .map(res => res.json());
   }
 
   getLocationsForSearch() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=locationForSearch')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=locationForSearch')
       .map(res => res.json());
   }
 
   getFullDetails() {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getFullDetails')
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getFullDetails')
       .map(res => res.json());
   }
 
   getCharges(id) {
-    return this.http.get('http://localhost/hair_b2b/src/api.php?f=getCharges&id=' + id)
+    return this.http.get('http://localhost/purchasinglanka/src/api.php?f=getCharges&id=' + id)
       .map(res => res.json());
   }
 }
